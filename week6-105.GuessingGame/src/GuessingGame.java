b012e7be8b01fe64dc128bb1d360ee261a457157
warglaivee <warglaivegm@gmail.com>
2019-11-01 17:20:16 +0100

    GuessingGame

--- week6-105.GuessingGame/src/GuessingGame.java
+++ week6-105.GuessingGame/src/GuessingGame.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class GuessingGame {
@@ -5,35 +6,55 @@ public class GuessingGame {
     private Scanner reader;
 
     public GuessingGame() {
-        // use only this scanner, othervise the tests do not work
         this.reader = new Scanner(System.in);
     }
 
     public void play(int lowerLimit, int upperLimit) {
         instructions(lowerLimit, upperLimit);
 
-        // write the guessing logic here
-
+        // write the game logic here
     }
 
     // implement here the methods isGreaterThan and average
-
     public void instructions(int lowerLimit, int upperLimit) {
         int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
 
         System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
 
-        System.out.println("I promise you that I can guess the number you are thinking with " + maxQuestions + " questions.");
+        System.out.println("I promise you that I can guess the number you are thinking of with " + maxQuestions + " questions.");
         System.out.println("");
-        System.out.println("Next I'll present you a series of questions. Answer them honestly.");
+        System.out.println("Next I'll present you with a series of questions. Answer them honestly.");
         System.out.println("");
+
+        while (lowerLimit != upperLimit) {
+            if (isGreaterThan(average(lowerLimit, upperLimit))) {
+                lowerLimit = average(lowerLimit, upperLimit);
+                if (upperLimit - lowerLimit <= 1) {
+                    lowerLimit = upperLimit;
+                    break;
+                }
+            } else {
+                upperLimit = average(lowerLimit, upperLimit);
+            }
+        }
+
+        System.out.println("The number you're thinking of is " + lowerLimit + ".");
     }
 
     // a helper method:
     public static int howManyTimesHalvable(int number) {
         // we create a base two logarithm  of the given value
-
         // Below we swap the base number to base two logarithms!
         return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+
+    public boolean isGreaterThan(int value) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Is your number greater than " + value + "?" + " (y/n)");
+        return scanner.nextLine().equals("y");
+    }
+
+    public int average(int firstNumber, int secondNumber) {
+        return (firstNumber + secondNumber) / 2;
     }
 }
